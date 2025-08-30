d21894df48a465cfac9146465e9ed52e2ef90f18
Vlada0604 <vladislavatorcinska@gmail.com>
2025-08-30 15:09:00 +0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Тут буде описана логіка роботи програми
+function showPage(pageId) {
+    // Отримуємо всі елементи з класом 'page'
+    const pages = document.querySelectorAll('.page');
+    
+    // Приховуємо всі сторінки
+    pages.forEach(page => {
+        page.classList.remove('active');
+    });
+
+    // Показуємо лише ту сторінку, яка була передана
+    const activePage = document.getElementById(pageId);
+    if (activePage) {
+        activePage.classList.add('active');
+    }
+}
